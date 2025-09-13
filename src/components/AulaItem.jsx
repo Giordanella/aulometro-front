e6c0f61036771c0046aa7e30464a1c2f8e4d6620
salvanescki <salvanescki.unq@gmail.com>
2025-09-13 05:11:43 -0300

    wip: formateo de aulas en la lista

--- src/components/AulaItem.jsx
+++ src/components/AulaItem.jsx
@@ -3,12 +3,12 @@ import "./styles/AulaItem.css";
 const AulaItem = ({ numeroAula, capacidad, ubicacion, cantidadComputadoras, tieneProyector, franjaHoraria }) => {
   return (
     <div className="aula-container">
-      <span className="aula-data numeroAula">{numeroAula}</span>
-      <span className="aula-data capacidad">{capacidad}</span>
-      <span className="aula-data numeroAula">{ubicacion}</span>
-      <span className="aula-data capacidad">{cantidadComputadoras}</span>
-      <span className="aula-data numeroAula">{tieneProyector ? "Sí" : "No"}</span>
-      <span className="aula-data capacidad">{franjaHoraria}</span>
+      <span className="numeroAula">Aula {numeroAula}</span>
+      <span className="aula-data">Capacidad: {capacidad}</span>
+      <span className="aula-data">{ubicacion}</span>
+      <span className="aula-data">Cantidad de computadoras: {cantidadComputadoras}</span>
+      <span className="aula-data">Con proyector: {tieneProyector ? "Sí" : "No"}</span>
+      <span className="franjaHoraria">Estado: {franjaHoraria}</span>
     </div>
   );
 };
