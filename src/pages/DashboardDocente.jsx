e922a89068ca6e1f2b59398e81b082cc10df6414
salvanescki <salvanescki.unq@gmail.com>
2025-09-13 05:29:59 -0300

    feat: agrego lista de aulas a dashboard docente

--- src/pages/DashboardDocente.jsx
+++ src/pages/DashboardDocente.jsx
@@ -1,13 +1,22 @@
 import { useAuth } from "../contexts/useAuth";
 import BotonPrimario from "../components/BotonPrimario";
+import ListaAulas from "../components/ListaAulas";
+import { useListaAulas } from "../hooks/useListaAulas";
+import "./styles/Home.css";
+import DataLoader from "../components/DataLoader.jsx";
 
 const DashboardDirectivo = () => {
   const { user, logout } = useAuth();
+  const { aulas, loading, error } = useListaAulas();
 
   return (
-    <div>
-      <h1>Bienvenido {user?.email}</h1>
-      <p>Tu rol es: {user?.role}</p>
+    <div className="home-container">
+      <h1>Bienvenido/a {user?.role}</h1>
+      <h2>Lista de Aulas</h2>
+      <DataLoader loading={loading} error={error}>
+        <ListaAulas aulas={aulas} />
+      </DataLoader>
+      <p>Has ingresado con tu email {user?.email}</p>
       <BotonPrimario onClick={logout}>Cerrar sesión</BotonPrimario>
     </div>
   );
