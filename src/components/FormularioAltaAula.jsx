7c74a6539534497306bc4d9a0fae5b5c64a0167e
salvanescki <salvanescki.unq@gmail.com>
2025-09-15 01:06:47 -0300

    refactor: campos formularios de aula

--- src/components/FormularioAltaAula.jsx
+++ src/components/FormularioAltaAula.jsx
@@ -3,7 +3,7 @@ import { createAula } from "../api/aulas";
 import "./styles/FormularioAlta.css";
 import BotonPrimario from "./BotonPrimario";
 import { validarCapacidad, validarNumeroAula, validarUbicacion, validarCantidadComputadoras, validarEstado } from "../utils/validarAula";
-import CampoFormulario from "./CampoFormulario";
+import CamposAula from "./CamposAula";
 
 const FormularioAltaAula = ({ onAulaCreada }) => {
 
@@ -41,67 +41,11 @@ const FormularioAltaAula = ({ onAulaCreada }) => {
     <div className="form-container">
       <form className="form" onSubmit={handleSubmit}>
         <h2>Alta de Aula</h2>
-
-        <CampoFormulario
-          placeholder="Número de aula"
-          name="numero"
-          type="number"
-          value={formData.numero}
-          onChange={handleChange}
-          error={errores.numero}
+        <CamposAula
+          formData={formData}
+          handleChange={handleChange}
+          errores={errores}
         />
-
-        <CampoFormulario
-          placeholder="Capacidad"
-          name="capacidad"
-          type="number"
-          value={formData.capacidad}
-          onChange={handleChange}
-          error={errores.capacidad}
-        />
-        
-        <CampoFormulario
-          placeholder="Ubicación"
-          name="ubicacion"
-          type="text"
-          value={formData.ubicacion}
-          onChange={handleChange}
-          error={errores.ubicacion}
-        />
-
-        <CampoFormulario
-          placeholder="Cantidad de computadoras"
-          name="computadoras"
-          type="number"
-          value={formData.computadoras}
-          onChange={handleChange}
-          error={errores.computadoras}
-        />
-
-        <CampoFormulario
-          placeholder="Estado"
-          name="estado"
-          type="select"
-          value={formData.estado}
-          onChange={handleChange}
-          error={errores.estado}
-        >
-          <option value="disponible">Disponible</option>
-          <option value="mantenimiento">En mantenimiento</option>
-          <option value="ocupada">No disponible</option>
-        </CampoFormulario>
-
-        <label className="form-label">
-          <input 
-            type="checkbox"
-            name="tieneProyector"
-            className="form-input"
-            checked={formData.tieneProyector}
-            onChange={handleChange}
-          />
-          ¿Tiene proyector?
-        </label>
-        {errores.tieneProyector && <p className="error-text">{errores.tieneProyector}</p>}
         <BotonPrimario>Dar de alta Aula</BotonPrimario>
       </form>
       {mensaje && <p className={`form-message ${tipoMensaje}`}>{mensaje}</p>}
--- src/components/FormularioEdicionAula.jsx
+++ src/components/FormularioEdicionAula.jsx
@@ -2,7 +2,7 @@ import { updateAulaById } from "../api/aulas";
 import { useFormulario } from "../hooks/useFormulario";
 import { validarNumeroAula, validarCapacidad, validarUbicacion, validarCantidadComputadoras, validarEstado } from "../utils/validarAula";
 import BotonPrimario from "./BotonPrimario";
-import CampoFormulario from "./CampoFormulario";
+import CamposAula from "./CamposAula";
 
 const FormularioEdicionAula = ({ aula, onAulaActualizada, onCancel }) => {
     
@@ -32,61 +32,11 @@ const FormularioEdicionAula = ({ aula, onAulaActualizada, onCancel }) => {
   return(
     <div className="form-container">
       <form className="form" onSubmit={handleSubmit}>
-        <CampoFormulario
-          label="Número de Aula"
-          name="numero"
-          type="number"
-          value={formData.numero}
-          onChange={handleChange}
-          error={errores.numero}
-        />
-        <CampoFormulario
-          label="Capacidad"
-          name="capacidad"
-          type="number"
-          value={formData.capacidad}
-          onChange={handleChange}
-          error={errores.capacidad}
-        />
-        <CampoFormulario
-          label="Ubicación"
-          name="ubicacion"
-          type="text"
-          value={formData.ubicacion}
-          onChange={handleChange}
-          error={errores.ubicacion}
-        />
-        <CampoFormulario
-          label="Cantidad de computadoras"
-          name="computadoras"
-          type="number"
-          value={formData.computadoras}
-          onChange={handleChange}
-          error={errores.computadoras}
-        />
-        <CampoFormulario
-          label="Estado"
-          name="estado"
-          type="select"
-          value={formData.estado}
-          onChange={handleChange}
-          error={errores.estado}
-        >
-          <option value="disponible">Disponible</option>
-          <option value="mantenimiento">En Mantenimiento</option>
-          <option value="ocupada">No disponible</option>
-        </CampoFormulario>
-        <label className="checkbox-label">
-          <input 
-            type="checkbox"
-            name="tieneProyector"
-            checked={formData.tieneProyector}
-            onChange={handleChange}
-          />
-                    ¿Tiene proyector?
-        </label>
-        {errores.tieneProyector && <p className="error-text">{errores.tieneProyector}</p>}
-                
+        <CamposAula
+          formData={formData}
+          handleChange={handleChange}
+          errores={errores}
+        />        
         {mensaje && <p className={`form-message ${tipoMensaje}`}>{mensaje}</p>}
 
         <div className="botones-edicion">
