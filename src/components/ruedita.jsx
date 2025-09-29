16cd5376cbef16775e4b695eca1fb08848bde5fa
Nacho <xxnachoxxt.m@gmail.com>
2025-09-29 15:02:07 -0300

    se cambia cargando... por rueda giratoria al esperar

---
+++ src/components/ruedita.jsx
@@ -0,0 +1,10 @@
+
+import CircularProgress from "@mui/material/CircularProgress";
+
+export default function Ruedita() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", padding: "1rem" }}>
+      <CircularProgress color="primary" />
+    </div>
+  );
+}
